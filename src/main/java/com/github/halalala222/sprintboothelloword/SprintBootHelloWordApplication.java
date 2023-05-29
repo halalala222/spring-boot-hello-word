4ed9a7db8413454629ec2a810d3d8f3e4177fbd8
halalala222 <1741196223@qq.com>
2023-05-29 08:58:07 +0800

    perf:remove unused import

--- src/main/java/com/github/halalala222/sprintboothelloword/SprintBootHelloWordApplication.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/SprintBootHelloWordApplication.java
@@ -2,8 +2,6 @@ package com.github.halalala222.sprintboothelloword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SprintBootHelloWordApplication {
