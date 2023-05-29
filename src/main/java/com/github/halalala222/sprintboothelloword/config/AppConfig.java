6b4ca9f551c720651cb03129f1791d7ce2f237a8
halalala222 <1741196223@qq.com>
2023-05-29 22:19:37 +0800

    perf:remove unless tag

--- src/main/java/com/github/halalala222/sprintboothelloword/config/AppConfig.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/config/AppConfig.java
@@ -8,12 +8,6 @@ import org.springframework.context.annotation.Configuration;
  *
  * @author : halalala222
  * @version : 1.0
- * @Project : sprint-boot-hello-word
- * @Package : com.github.halalala222.sprintboothelloword.config
- * @ClassName : .java
- * @createTime : 2023/5/29 9:37
- * @Email : 1741196223@qq.com
- * @Description :
  */
 @Configuration
 public class AppConfig {
