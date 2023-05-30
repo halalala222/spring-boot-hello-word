cd616d3fb1b410aa085793ed5314fbcd6a86a07c
halalala222 <1741196223@qq.com>
2023-05-30 10:06:21 +0800

    feat:remove unused import

--- src/main/java/com/github/halalala222/sprintboothelloword/handler/GlobalExceptionHandler.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/handler/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.github.halalala222.sprintboothelloword.handler;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
--- src/main/java/com/github/halalala222/sprintboothelloword/helloWorld/HelloWorld.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/helloWorld/HelloWorld.java
@@ -1,6 +1,5 @@
 package com.github.halalala222.sprintboothelloword.helloWorld;
 
-import com.github.halalala222.sprintboothelloword.config.AppConfig;
 import com.github.halalala222.sprintboothelloword.handler.BaseException;
 import com.github.halalala222.sprintboothelloword.handler.ResponseCode;
 import org.springframework.web.bind.annotation.GetMapping;
