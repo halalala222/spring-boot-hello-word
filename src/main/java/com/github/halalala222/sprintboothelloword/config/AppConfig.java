cbd4dc30e4676f8c658c5666bf506a12bf98ed53
halalala222 <1741196223@qq.com>
2023-06-01 10:01:27 +0800

    perf:remove jwt config

--- src/main/java/com/github/halalala222/sprintboothelloword/config/AppConfig.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.github.halalala222.sprintboothelloword.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,14 +12,8 @@ import org.springframework.context.annotation.Configuration;
  * @version : 1.0
  */
 @Configuration
-@Getter
 public class AppConfig {
     @Value("${person.name}")
     private String name;
 
-    @Value("${jwt.expire-time}")
-    private int expireTime;
-
-    @Value("${jwt.secret}")
-    private String secret;
 }
