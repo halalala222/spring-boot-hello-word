d24a2d611dbd0c788fe562a8f8de938864df5695
halalala222 <1741196223@qq.com>
2023-06-01 10:04:49 +0800

    feat:global exception handler add HttpRequestMethodNotSupportedException handle

--- src/main/java/com/github/halalala222/sprintboothelloword/handler/GlobalExceptionHandler.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.github.halalala222.sprintboothelloword.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +21,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Response<Void> error(Exception e) {
+    public <T> Response<T> error(Exception e) {
         log.error("[run time error : ]", e);
         return Response.serviceError();
     }
@@ -28,13 +29,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NullPointerException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Response<Void> error(NullPointerException e) {
+    public <T> Response<T> error(NullPointerException e) {
         log.error("[null pointer error : ]", e);
         return Response.serviceError();
     }
 
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<Response<Void>> error(BaseException e) {
+    public <T> ResponseEntity<Response<T>> error(BaseException e) {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
         ResponseCode code = e.getCode();
         if (code.getCode() == ResponseCode.JWT_ERROR.getCode()) {
@@ -47,5 +48,13 @@ public class GlobalExceptionHandler {
             log.error("[internal service error : ]", e);
         }
         return ResponseEntity.status(httpStatus).body(Response.errorWithCode(code));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public <T> Response<T> info(HttpRequestMethodNotSupportedException e) {
+        log.info("[http method not found error : ]", e);
+        return null;
     }
 }
