c7f09b94e279d7c4eee0de88ef12a74c4244bc35
halalala222 <1741196223@qq.com>
2023-06-01 10:05:11 +0800

    feat:add user dataStruct

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/controller/User.java
@@ -0,0 +1,15 @@
+package com.github.halalala222.sprintboothelloword.controller;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
