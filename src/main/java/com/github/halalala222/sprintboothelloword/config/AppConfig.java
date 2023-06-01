50209934b52a12ac948e25ae8661f769678cb769
halalala222 <1741196223@qq.com>
2023-06-01 10:15:08 +0800

    perf:remove unused import

--- src/main/java/com/github/halalala222/sprintboothelloword/config/AppConfig.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/config/AppConfig.java
@@ -1,8 +1,6 @@
 package com.github.halalala222.sprintboothelloword.config;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
