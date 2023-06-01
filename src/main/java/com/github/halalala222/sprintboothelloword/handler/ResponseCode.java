7a7d1495dc8dd2460244873e7981468c93780092
halalala222 <1741196223@qq.com>
2023-06-01 10:04:12 +0800

    feat:add un auth error code

--- src/main/java/com/github/halalala222/sprintboothelloword/handler/ResponseCode.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/handler/ResponseCode.java
@@ -1,6 +1,7 @@
 package com.github.halalala222.sprintboothelloword.handler;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,7 +14,8 @@ public enum ResponseCode {
     OK(1, "ok"),
     SERVICE_ERROR(2, "internal error"),
     JWT_ERROR(1001, "token error"),
-    USER_NOT_FOUND_ERROR(1002, "user not found");
+    USER_NOT_FOUND_ERROR(1002, "user not found"),
+    USER_UN_AUTHORIZATION(1003,"auth error");
     private final int code;
     private final String message;
 
