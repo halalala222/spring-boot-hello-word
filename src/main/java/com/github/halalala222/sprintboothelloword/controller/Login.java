ec359baf20984585e5b5e59ea318a00a79a086c0
halalala222 <1741196223@qq.com>
2023-06-01 10:42:44 +0800

    perf:change use controller to dependency injection

--- src/main/java/com/github/halalala222/sprintboothelloword/controller/Login.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/controller/Login.java
@@ -3,6 +3,8 @@ package com.github.halalala222.sprintboothelloword.controller;
 import com.github.halalala222.sprintboothelloword.handler.BaseException;
 import com.github.halalala222.sprintboothelloword.handler.Response;
 import com.github.halalala222.sprintboothelloword.handler.ResponseCode;
+import com.github.halalala222.sprintboothelloword.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -14,6 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/login")
 public class Login {
+    JwtUtils jwtUtils;
+
+    public Login(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
     @PostMapping
     public Response<String> LoginController(@RequestBody User user) throws BaseException {
         if (user.getUserName() == null || user.getPassword() == null) {
@@ -22,6 +30,7 @@ public class Login {
         if (!(user.getUserName().equals("liooooo") && user.getPassword().equals("admin"))) {
             throw new BaseException(ResponseCode.USER_NOT_FOUND_ERROR);
         }
-        return Response.successWithoutData();
+        String token = jwtUtils.generateToken(1L);
+        return Response.successWithData(token);
     }
 }
