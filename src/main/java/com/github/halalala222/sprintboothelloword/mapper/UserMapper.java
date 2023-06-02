fd41273af5cc6960ba6825e0508ae7c0f87d18e3
halalala222 <1741196223@qq.com>
2023-06-02 16:36:22 +0800

    feat:add userMapper

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.github.halalala222.sprintboothelloword.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.halalala222.sprintboothelloword.entity.User;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+@Component
+public interface UserMapper extends BaseMapper<User> {
+}
