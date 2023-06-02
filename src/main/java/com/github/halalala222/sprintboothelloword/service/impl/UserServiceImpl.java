eb87258d0220edd406e9dd9dff276cf2d9be1de0
halalala222 <1741196223@qq.com>
2023-06-02 17:04:47 +0800

    feat:add userServiceImpl implement UserService interface

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.github.halalala222.sprintboothelloword.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.halalala222.sprintboothelloword.entity.User;
+import com.github.halalala222.sprintboothelloword.mapper.UserMapper;
+import com.github.halalala222.sprintboothelloword.service.UserService;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+}
