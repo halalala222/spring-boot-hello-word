b928e624ef5a229993be9769e5452e7c19b3c375
halalala222 <1741196223@qq.com>
2023-06-02 16:36:56 +0800

    feat:Application add MapperScan annotation

--- src/main/java/com/github/halalala222/sprintboothelloword/SprintBootHelloWordApplication.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/SprintBootHelloWordApplication.java
@@ -1,9 +1,11 @@
 package com.github.halalala222.sprintboothelloword;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.github.halalala222.sprintboothelloword.mapper")
 public class SprintBootHelloWordApplication {
 
     public static void main(String[] args) {
