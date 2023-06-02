f98dfb50377467b967715b025bb0566cb182437f
halalala222 <1741196223@qq.com>
2023-06-02 17:04:11 +0800

    feat:add userService interface

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/service/UserService.java
@@ -0,0 +1,13 @@
+package com.github.halalala222.sprintboothelloword.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.halalala222.sprintboothelloword.entity.User;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+public interface UserService extends IService<User> {
+}
