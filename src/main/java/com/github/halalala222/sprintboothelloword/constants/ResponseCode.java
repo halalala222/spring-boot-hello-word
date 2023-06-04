83a0951a32e2c43ae9b71b55aab72525c07e51ea
halalala222 <1741196223@qq.com>
2023-06-04 16:39:25 +0800

    feat:response code add USER_EXITED code

--- src/main/java/com/github/halalala222/sprintboothelloword/constants/ResponseCode.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/constants/ResponseCode.java
@@ -1,6 +1,7 @@
 package com.github.halalala222.sprintboothelloword.constants;
 
 import lombok.Getter;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -14,7 +15,8 @@ public enum ResponseCode {
     JWT_ERROR(1001, "token error"),
     USER_NOT_FOUND_ERROR(1002, "user not found"),
     USER_UN_AUTHORIZATION(1003, "auth error"),
-    REQUEST_DATA_ERROR(1004, "请求参数错误");
+    REQUEST_DATA_ERROR(1004, "请求参数错误"),
+    USER_EXITED(1005, "用户名已经注册过");
     private final int code;
     private final String message;
 
