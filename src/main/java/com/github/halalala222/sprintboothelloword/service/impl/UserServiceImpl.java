622b6c97e64e2ffeb2b22443294475eeac37bdbb
halalala222 <1741196223@qq.com>
2023-06-04 16:39:57 +0800

    fix:UserServiceImpl add Service annotation

--- src/main/java/com/github/halalala222/sprintboothelloword/service/impl/UserServiceImpl.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.halalala222.sprintboothelloword.entity.User;
 import com.github.halalala222.sprintboothelloword.mapper.UserMapper;
 import com.github.halalala222.sprintboothelloword.service.UserService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,5 +12,6 @@ import com.github.halalala222.sprintboothelloword.service.UserService;
  * @author : halalala222
  * @version : 1.0
  */
+@Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 }
