fad7072eda79955a4de1f68f2d28b7a4349b85cd
halalala222 <1741196223@qq.com>
2023-06-04 14:48:02 +0800

    feat:change login request body to loginUser class

--- src/main/java/com/github/halalala222/sprintboothelloword/controller/Login.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/controller/Login.java
@@ -4,6 +4,7 @@ import com.github.halalala222.sprintboothelloword.exception.BaseException;
 import com.github.halalala222.sprintboothelloword.handler.Response;
 import com.github.halalala222.sprintboothelloword.constants.ResponseCode;
 import com.github.halalala222.sprintboothelloword.utils.JwtUtils;
+import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -22,7 +23,7 @@ public class Login {
     }
 
     @PostMapping
-    public Response<String> LoginController(@RequestBody User user) throws BaseException {
+    public Response<String> LoginController(@RequestBody LoginUser user) throws BaseException {
         if (user.getUserName() == null || user.getPassword() == null) {
             throw new BaseException(ResponseCode.REQUEST_DATA_ERROR);
         }
@@ -32,4 +33,10 @@ public class Login {
         String token = jwtUtils.generateToken(1L);
         return Response.successWithData(token);
     }
+}
+
+@Data
+class LoginUser {
+    private String userName;
+    private String password;
 }
