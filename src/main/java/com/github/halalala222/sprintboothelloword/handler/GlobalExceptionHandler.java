139895f98b395acf810d137d6374698164bf3c65
halalala222 <1741196223@qq.com>
2023-06-04 22:17:54 +0800

    feat:handle MethodArgumentNotValidException exception

--- src/main/java/com/github/halalala222/sprintboothelloword/handler/GlobalExceptionHandler.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/handler/GlobalExceptionHandler.java
@@ -3,13 +3,16 @@ package com.github.halalala222.sprintboothelloword.handler;
 import com.github.halalala222.sprintboothelloword.constants.ResponseCode;
 import com.github.halalala222.sprintboothelloword.exception.BaseException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -58,5 +61,17 @@ public class GlobalExceptionHandler {
     public <T> Response<T> info(HttpRequestMethodNotSupportedException e) {
         log.info("[http method not found error : ]", e);
         return null;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public <T> Response<T> info(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrorList = e.getFieldErrors();
+        List<String> collect = fieldErrorList.stream().map(
+                DefaultMessageSourceResolvable::getDefaultMessage
+        ).toList();
+        String unValidMessage = collect.toString();
+        return Response.errorWithCode(ResponseCode.REQUEST_PARAM_ERROR.getResponseCodeSetMessage(unValidMessage.substring(1, unValidMessage.length() - 1)));
     }
 }
