3a94fe2cfcc5534f5c5f5604ff43cd4641782c38
halalala222 <1741196223@qq.com>
2023-06-05 09:44:10 +0800

    feat:add check user password strength method

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/utils/CheckPassword.java
@@ -0,0 +1,16 @@
+package com.github.halalala222.sprintboothelloword.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+public class CheckPassword {
+
+    private static final String PASSWORD_PATTERN = "^(?![A-Za-z0-9]+$)(?![a-z0-9\\\\W]+$)(?![A-Za-z\\\\W]+$)(?![A-Z0-9\\\\W]+$)[a-zA-Z0-9\\\\W]{8,}$";
+
+    public static boolean check(String password) {
+        return password.matches(PASSWORD_PATTERN);
+    }
+}
