39afb7f0b6a5983890019c3386035419659db0be
halalala222 <1741196223@qq.com>
2023-06-06 19:35:35 +0800

    feat:add update user profile api

--- src/main/java/com/github/halalala222/sprintboothelloword/controller/UserController.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.github.halalala222.sprintboothelloword.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.halalala222.sprintboothelloword.constants.RedisConstants;
 import com.github.halalala222.sprintboothelloword.constants.ResponseCode;
@@ -11,14 +12,14 @@ import com.github.halalala222.sprintboothelloword.service.UserService;
 import com.github.halalala222.sprintboothelloword.utils.JwtUtils;
 import com.github.halalala222.sprintboothelloword.utils.RedisUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -68,6 +69,21 @@ public class UserController {
 
         return Response.successWithData(responseData);
     }
+
+    @PutMapping("/profile")
+    public Response<Void> updateUserProfile(@RequestBody @Validated UpdateUserProfile updateUserProfile, HttpServletRequest request) throws BaseException {
+        Long userId = jwtUtils.getUserIdFromToken(jwtUtils.getTokenFromRequestHeader(request));
+        LambdaUpdateWrapper<User> userUpdateWrapper = new LambdaUpdateWrapper<>();
+        userUpdateWrapper.eq(User::getId, userId).
+                set(User::getName, updateUserProfile.getName()).
+                set(User::getSignature, updateUserProfile.getSignature());
+        boolean isUpdated = userService.update(userUpdateWrapper);
+        if (!isUpdated) {
+            throw new BaseException(ResponseCode.SERVICE_ERROR);
+        }
+        redisUtils.delete(RedisConstants.getFullKey(RedisConstants.USER_PROFILE_KEY_PREFIX, userId.toString()));
+        return Response.successWithoutData();
+    }
 }
 
 @Getter
@@ -79,4 +95,15 @@ class UserProfile {
     private Long id;
     private String signature;
     private String email;
+}
+
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+class UpdateUserProfile {
+    @NotEmpty(message = "签名不能为空")
+    private String signature;
+    @NotEmpty(message = "姓名不能为空")
+    private String name;
 }
