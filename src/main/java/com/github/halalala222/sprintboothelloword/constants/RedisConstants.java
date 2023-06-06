138502d728643826c700e1aeb8d9ef28d527cc6c
halalala222 <1741196223@qq.com>
2023-06-06 15:25:35 +0800

    feat:add user profile redis constants

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/constants/RedisConstants.java
@@ -0,0 +1,20 @@
+package com.github.halalala222.sprintboothelloword.constants;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+public class RedisConstants {
+    public static String BASIC_PREFIX = "spring-boot-hello-world";
+    public static String USER_PROFILE_KEY_PREFIX = "user-profile";
+
+    public static long USER_PROFILE_TTL = 60 * 60 * 5;
+
+    public static String getFullKey(String key, String unique) {
+        return BASIC_PREFIX + "-" + key + "-" + unique;
+    }
+}
