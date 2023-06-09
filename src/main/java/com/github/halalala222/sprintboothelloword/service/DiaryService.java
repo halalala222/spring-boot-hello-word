33c8f1953a8ecb890e84ab73f23ee32d0b2eb53b
halalala222 <1741196223@qq.com>
2023-06-09 19:23:17 +0800

    feat:add diaryService interface

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/service/DiaryService.java
@@ -0,0 +1,13 @@
+package com.github.halalala222.sprintboothelloword.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.halalala222.sprintboothelloword.entity.Diary;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+public interface DiaryService extends IService<Diary> {
+}
