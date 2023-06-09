a8209459126223dd07ca192beae57ac28974e51f
halalala222 <1741196223@qq.com>
2023-06-09 19:23:02 +0800

    feat:add diaryMapper

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/mapper/DiaryMapper.java
@@ -0,0 +1,13 @@
+package com.github.halalala222.sprintboothelloword.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.halalala222.sprintboothelloword.entity.Diary;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+public interface DiaryMapper extends BaseMapper<Diary> {
+}
