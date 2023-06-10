01e4c71181509f8ccd67d5ac09b35859a757b293
halalala222 <1741196223@qq.com>
2023-06-10 16:47:18 +0800

    feat:diary mapper use select annotation to left join table

--- src/main/java/com/github/halalala222/sprintboothelloword/mapper/DiaryMapper.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/mapper/DiaryMapper.java
@@ -2,6 +2,11 @@ package com.github.halalala222.sprintboothelloword.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.halalala222.sprintboothelloword.entity.Diary;
+import com.github.halalala222.sprintboothelloword.entity.DiaryDTO;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,5 +14,8 @@ import com.github.halalala222.sprintboothelloword.entity.Diary;
  * @author : halalala222
  * @version : 1.0
  */
+@Component
 public interface DiaryMapper extends BaseMapper<Diary> {
+    @Select("SELECT diary.content,diary.id,user.name AS user_name FROM diary LEFT JOIN user ON diary.user_id = user.id")
+    List<DiaryDTO> getDiaries();
 }
