1d02fbd3998652486b36c6cc6a8881677e12c786
halalala222 <1741196223@qq.com>
2023-06-28 20:25:33 +0800

    feat:UserDiaryLikeMapper add Component annotation

--- src/main/java/com/github/halalala222/sprintboothelloword/mapper/UserDiaryLikeMapper.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/mapper/UserDiaryLikeMapper.java
@@ -2,6 +2,7 @@ package com.github.halalala222.sprintboothelloword.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.halalala222.sprintboothelloword.entity.UserDiaryLike;
+import org.springframework.stereotype.Component;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,5 +10,6 @@ import com.github.halalala222.sprintboothelloword.entity.UserDiaryLike;
  * @author : halalala222
  * @version : 1.0
  */
+@Component
 public interface UserDiaryLikeMapper extends BaseMapper<UserDiaryLike> {
 }
