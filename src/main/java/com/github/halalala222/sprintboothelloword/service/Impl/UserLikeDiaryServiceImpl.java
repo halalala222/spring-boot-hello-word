edb915c43f5390c655f85487a743c987a4be89b1
halalala222 <1741196223@qq.com>
2023-07-06 14:39:06 +0800

    feat:add user like diary service

---
+++ src/main/java/com/github/halalala222/sprintboothelloword/service/Impl/UserLikeDiaryServiceImpl.java
@@ -0,0 +1,50 @@
+package com.github.halalala222.sprintboothelloword.service.Impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.github.halalala222.sprintboothelloword.dao.UserDiaryLikeDao;
+import com.github.halalala222.sprintboothelloword.dto.UserLikeDiaryDTO;
+import com.github.halalala222.sprintboothelloword.entity.UserDiaryLike;
+import com.github.halalala222.sprintboothelloword.exception.BaseException;
+import com.github.halalala222.sprintboothelloword.service.UserLikeDiaryService;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+@Service
+public class UserLikeDiaryServiceImpl implements UserLikeDiaryService {
+    private final UserDiaryLikeDao userDiaryLikeDao;
+
+    public UserLikeDiaryServiceImpl(UserDiaryLikeDao userDiaryLikeDao) {
+        this.userDiaryLikeDao = userDiaryLikeDao;
+    }
+
+    @Override
+    public void UserLikeDiary(UserLikeDiaryDTO userLikeDiaryDTO) throws BaseException {
+        UserDiaryLike userDiaryLike = UserDiaryLike.builder().
+                diaryId(userLikeDiaryDTO.getDiaryId()).
+                userId(userLikeDiaryDTO.getUserId())
+                .build();
+        try {
+            userDiaryLikeDao.save(userDiaryLike);
+        } catch (DuplicateKeyException e) {
+            LambdaQueryWrapper<UserDiaryLike> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.
+                    eq(UserDiaryLike::getUserId, userLikeDiaryDTO.getDiaryId()).
+                    eq(UserDiaryLike::getDiaryId, userLikeDiaryDTO.getDiaryId());
+            UserDiaryLike data = userDiaryLikeDao.getOne(queryWrapper);
+            if (data != null) {
+                userDiaryLikeDao.remove(queryWrapper);
+            } else {
+                userDiaryLikeDao.recoveryLogicDelete(
+                        userLikeDiaryDTO.getUserId(),
+                        userLikeDiaryDTO.getDiaryId()
+                );
+            }
+        }
+    }
+}
---
+++ src/main/java/com/github/halalala222/sprintboothelloword/service/UserLikeDiaryService.java
@@ -0,0 +1,13 @@
+package com.github.halalala222.sprintboothelloword.service;
+
+import com.github.halalala222.sprintboothelloword.dto.UserLikeDiaryDTO;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : halalala222
+ * @version : 1.0
+ */
+public interface UserLikeDiaryService {
+    void UserLikeDiary(UserLikeDiaryDTO userLikeDiaryDTO);
+}
