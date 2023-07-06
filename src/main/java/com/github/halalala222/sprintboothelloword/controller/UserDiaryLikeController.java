646666657669be70f56e54a2d5b947e1c82cdead
halalala222 <1741196223@qq.com>
2023-07-06 14:39:43 +0800

    refactor:change user diary like controller to use user diary like service

--- src/main/java/com/github/halalala222/sprintboothelloword/controller/UserDiaryLikeController.java
+++ src/main/java/com/github/halalala222/sprintboothelloword/controller/UserDiaryLikeController.java
@@ -1,15 +1,14 @@
 package com.github.halalala222.sprintboothelloword.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.github.halalala222.sprintboothelloword.dao.UserDiaryLikeDao;
-import com.github.halalala222.sprintboothelloword.entity.UserDiaryLike;
+import com.github.halalala222.sprintboothelloword.dto.UserLikeDiaryDTO;
 import com.github.halalala222.sprintboothelloword.exception.BaseException;
 import com.github.halalala222.sprintboothelloword.handler.Response;
+import com.github.halalala222.sprintboothelloword.service.UserLikeDiaryService;
 import com.github.halalala222.sprintboothelloword.utils.JwtUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,13 +21,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user/diary")
 public class UserDiaryLikeController {
-    private final UserDiaryLikeDao userDiaryLikeDao;
+    private final UserLikeDiaryService userLikeDiaryService;
 
     private final JwtUtils jwtUtils;
 
     @Autowired
-    public UserDiaryLikeController(UserDiaryLikeDao userDiaryLikeDao, JwtUtils jwtUtils) {
-        this.userDiaryLikeDao = userDiaryLikeDao;
+    public UserDiaryLikeController(UserLikeDiaryService userLikeDiaryService, JwtUtils jwtUtils) {
+        this.userLikeDiaryService = userLikeDiaryService;
         this.jwtUtils = jwtUtils;
     }
 
@@ -37,24 +36,12 @@ public class UserDiaryLikeController {
                                         @RequestBody @Validated UserLikeDiary userLikeDiary)
             throws BaseException {
         Long userId = jwtUtils.getUserIdFromToken(jwtUtils.getTokenFromRequestHeader(httpServletRequest));
-        UserDiaryLike userDiaryLike = UserDiaryLike.builder().
-                diaryId(userLikeDiary.getDiaryId()).
-                userId(userId).build();
-        try {
-            userDiaryLikeDao.save(userDiaryLike);
-        } catch (DuplicateKeyException e) {
-            LambdaQueryWrapper<UserDiaryLike> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.
-                    eq(UserDiaryLike::getUserId, userId).
-                    eq(UserDiaryLike::getDiaryId, userLikeDiary.getDiaryId());
-            UserDiaryLike data = userDiaryLikeDao.getOne(queryWrapper);
-            if (data != null) {
-                userDiaryLikeDao.remove(queryWrapper);
-            } else {
-                userDiaryLikeDao.recoveryLogicDelete(userId, userLikeDiary.getDiaryId());
-            }
-
-        }
+        userLikeDiaryService.UserLikeDiary(
+                UserLikeDiaryDTO.
+                        builder().diaryId(userLikeDiary.getDiaryId()).
+                        userId(userId)
+                        .build()
+        );
 
         return Response.successWithoutData();
     }
@@ -65,5 +52,6 @@ public class UserDiaryLikeController {
 @NoArgsConstructor
 @Builder
 class UserLikeDiary {
+    @NotEmpty(message = "diaryId 不能为空")
     private Long diaryId;
 }
